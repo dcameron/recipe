f3e91256d4f967b3a329df342e30ab1614b1416c
David Cameron <david@cadeyrn.us>
2015-11-13 17:36:57 -0600

    Updated IngredientFieldTest to use 8.x entity APIs.

--- modules/ingredient/src/Tests/IngredientFieldTest.php
+++ modules/ingredient/src/Tests/IngredientFieldTest.php
@@ -21,7 +21,7 @@ class IngredientFieldTest extends WebTestBase {
    *
    * @var array
    */
-  public static $modules = array('ingredient');
+  public static $modules = array('ingredient', 'node');
 
   /**
    * A test user with administrative privileges.
@@ -37,7 +37,7 @@ class IngredientFieldTest extends WebTestBase {
     $content_type = $this->drupalCreateContentType(array('type' => 'test_bundle'));
 
     // Create and log in the admin user.
-    $this->admin_user = $this->drupalCreateUser(array('create test_bundle content', 'administer site configuration'));
+    $this->admin_user = $this->drupalCreateUser(array('create test_bundle content', 'add ingredient', 'view ingredient', 'administer site configuration'));
     $this->drupalLogin($this->admin_user);
   }
 
@@ -45,20 +45,18 @@ class IngredientFieldTest extends WebTestBase {
    * Tests ingredient field settings.
    */
   public function testIngredientFieldSettings() {
-    // Create the field.
-    $field = array(
-      'cardinality' => -1,
-      'field_name' => 'ingredient',
-      'module' => 'recipe',
-      'settings' => array(
-        'ingredient_name_normalize' => 1,
-      ),
-      'type' => 'ingredient_reference',
-    );
-    field_create_field($field);
+    // Create an ingredient field on the test_bundle node type.
+    $field_name = strtolower($this->randomMachineName());
+    $storage_settings = [
+      'ingredient_name_normalize' => 1,
+    ];
+    $field_settings = [
+      'default_unit' => 'cup',
+    ];
+    $this->createIngredientField($field_name, 'node', 'test_bundle', $storage_settings, $field_settings);
 
-    // Create the field instance.
-    $instance = array(
+    // 7.x field instance settings retained for display settings.
+    /*$instance = array(
       'bundle' => 'test_bundle',
       'display' => array(
         'default' => array(
@@ -84,8 +82,7 @@ class IngredientFieldTest extends WebTestBase {
         'type' => 'recipe_ingredient_autocomplete',
         'weight' => 0,
       ),
-    );
-    field_create_instance($instance);
+    );*/
 
     $edit = array(
       'title[0][value]' => $this->randomMachineName(16),
@@ -104,4 +101,75 @@ class IngredientFieldTest extends WebTestBase {
     // Assert that the normalized ingredient name can be found on the node page.
     $this->assertText('test ingredient', 'Found the normalized ingredient name.');
   }
+
+  /**
+   * Creates a new ingredient field.
+   *
+   * @param string $name
+   *   The name of the new field (all lowercase), exclude the "field_" prefix.
+   * @param string $entity_type
+   *   The entity type.
+   * @param string $bundle
+   *   The bundle that this field will be added to.
+   * @param array $storage_settings
+   *   A list of field storage settings that will be added to the defaults.
+   * @param array $field_settings
+   *   A list of instance settings that will be added to the instance defaults.
+   * @param array $widget_settings
+   *   A list of widget settings that will be added to the widget defaults.
+   */
+  protected function createIngredientField($name, $entity_type, $bundle, $storage_settings = array(), $field_settings = array(), $widget_settings = array()) {
+    $field_storage = entity_create('field_storage_config', array(
+      'entity_type' => $entity_type,
+      'field_name' => $name,
+      'type' => 'ingredient',
+      'settings' => $storage_settings,
+      'cardinality' => !empty($storage_settings['cardinality']) ? $storage_settings['cardinality'] : 1,
+    ));
+    $field_storage->save();
+
+    $this->attachIngredientField($name, $entity_type, $bundle, $field_settings, $widget_settings);
+    return $field_storage;
+  }
+
+  /**
+   * Attaches an ingredient field to an entity.
+   *
+   * @param string $name
+   *   The name of the new field (all lowercase), exclude the "field_" prefix.
+   * @param string $entity_type
+   *   The entity type this field will be added to.
+   * @param string $bundle
+   *   The bundle this field will be added to.
+   * @param array $field_settings
+   *   A list of field settings that will be added to the defaults.
+   * @param array $widget_settings
+   *   A list of widget settings that will be added to the widget defaults.
+   */
+  protected function attachIngredientField($name, $entity_type, $bundle, $field_settings = array(), $widget_settings = array()) {
+    $field = array(
+      'field_name' => $name,
+      'label' => $name,
+      'entity_type' => $entity_type,
+      'bundle' => $bundle,
+      'required' => !empty($field_settings['required']),
+      'settings' => $field_settings,
+    );
+    entity_create('field_config', $field)->save();
+
+    entity_get_form_display($entity_type, $bundle, 'default')
+      ->setComponent($name, array(
+        'type' => 'ingredient_autocomplete',
+        'settings' => $widget_settings,
+      ))
+      ->save();
+    // Assign display settings.
+    entity_get_display($entity_type, $bundle, 'default')
+      ->setComponent($name, array(
+        'label' => 'hidden',
+        'type' => 'ingredient_default',
+      ))
+      ->save();
+  }
+
 }
