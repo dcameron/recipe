9f65b633a9decd97573b2348c4f6ce708d7ec514
David Cameron <david@cadeyrn.us>
2015-11-19 02:29:09 -0600

    Converted the recipe_ingredient field to configuration.

--- src/Tests/RecipeNodeTest.php
+++ src/Tests/RecipeNodeTest.php
@@ -46,11 +46,11 @@ class RecipeNodeTest extends RecipeTestBase {
       'recipe_notes[0][value]' => $notes,
       'recipe_instructions[0][value]' => $instructions,
 /*      'recipe_prep_time[0][value]' => $preptime,
-      'recipe_cook_time[0][value]' => $cooktime,
+      'recipe_cook_time[0][value]' => $cooktime,*/
       'recipe_ingredient[0][quantity]' => $ing_0_quantity,
       'recipe_ingredient[0][unit_key]' => $ing_0_unit,
-      'recipe_ingredient[0][name]' => $ing_0_name,
-      'recipe_ingredient[0][note]' => $ing_0_note,*/
+      'recipe_ingredient[0][target_id]' => $ing_0_name,
+      'recipe_ingredient[0][note]' => $ing_0_note,
     );
 
     // Post the values to the node form.
