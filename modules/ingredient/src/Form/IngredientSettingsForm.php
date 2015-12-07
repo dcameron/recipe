d6ce6ea6af6b2167a06b0c05bebbcc877baa4f43
David Cameron <david@cadeyrn.us>
2015-12-07 10:27:39 -0600

    Removed old Ingredient system of measurement settings and code.

--- modules/ingredient/src/Form/IngredientSettingsForm.php
+++ modules/ingredient/src/Form/IngredientSettingsForm.php
@@ -44,27 +44,6 @@ class IngredientSettingsForm extends ConfigFormBase {
       '#required' => TRUE,
     ];
 
-    // System of measurement section
-    $form['system_of_measurement'] = array(
-      '#type' => 'fieldset',
-      '#title' => t('System of measurement'),
-    );
-    $form['system_of_measurement']['ingredient_preferred_system_of_measure'] = array(
-      '#type' => 'radios',
-      '#title' => $this->t('Preferred system of measure'),
-      '#default_value' => $config->get('ingredient_preferred_system_of_measure'),
-      '#options' => array($this->t('U.S. customary units'), $this->t('SI/Metric')),
-      '#description' => $this->t('Which system of measure should be preferred where it is ambiguous.'),
-      '#required' => TRUE,
-    );
-    $form['system_of_measurement']['ingredient_preferred_system_of_measure_limit'] = array(
-      '#type' => 'checkbox',
-      '#title' => $this->t('Limit UI to the preferred system of measure'),
-      '#default_value' => $config->get('ingredient_preferred_system_of_measure_limit'),
-      '#return_value' => 1,
-      '#description' => $this->t('Limit unit selectbox to only preferred system of measure.  Does not affect import routines.'),
-    );
-
     return parent::buildForm($form, $form_state);
   }
 
@@ -75,8 +54,6 @@ class IngredientSettingsForm extends ConfigFormBase {
     $values = $form_state->getValues();
     $this->config('ingredient.settings')
       ->set('ingredient_name_normalize', $values['ingredient_name_normalize'])
-      ->set('ingredient_preferred_system_of_measure', $values['ingredient_preferred_system_of_measure'])
-      ->set('ingredient_preferred_system_of_measure_limit', $values['ingredient_preferred_system_of_measure_limit'])
       ->save();
     parent::submitForm($form, $form_state);
   }
