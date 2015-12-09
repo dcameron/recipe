6d7fae3c43484cd0a9c11dea2e887d7fe4b06b1e
David Cameron <david@cadeyrn.us>
2015-12-08 18:39:16 -0600

    Ordered the Ingredient class use definitions in alphabetical order.

--- modules/ingredient/src/Entity/Ingredient.php
+++ modules/ingredient/src/Entity/Ingredient.php
@@ -6,13 +6,12 @@
 
 namespace Drupal\ingredient\Entity;
 
-use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\Entity\ContentEntityBase;
-use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\ingredient\IngredientInterface;
-use Drupal\user\UserInterface;
 
 /**
  * Defines the Ingredient entity.
